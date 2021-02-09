4c60e6a852a62a406e66aced2cb45dcbb1d93c6e
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-02-09 16:21:28 +0900

    Edit some router function

--- src/controllers/globalControllers.js
+++ src/controllers/globalControllers.js
@@ -1,12 +1,22 @@
 import fs from "fs";
-
+let filePath;
 export const getHome = (req, res) => {
   res.render("home");
 };
 export const postHome = (req, res) => {
-  const filePath = req.file.path;
+  const {
+    file: { path },
+  } = req;
+  fs.readFile(`${path}`, "utf8", (err, data) => {
+    if (err) throw err;
+    filePath = path;
+    res.redirect("/read");
+  });
+};
+
+export const getRead = (req, res) => {
   fs.readFile(`${filePath}`, "utf8", (err, data) => {
     if (err) throw err;
-    res.render("home", { data });
+    res.render("read", { data });
   });
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getHome, postHome } from "../controllers/globalControllers";
+import { getHome, getRead, postHome } from "../controllers/globalControllers";
 import { uploadText } from "../middlewares/localMiddleware";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", getHome);
 globalRouter.post("/", uploadText, postHome);
+globalRouter.get("/read", getRead);
 
 export default globalRouter;
